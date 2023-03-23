8cef450e5e916fb36679b813c16c07ee529e1ea9
Sérgio Carvalho Carvalho <sergiocarvalho/48>
2023-03-23 16:00:53 -0300

    Add Navbar

---
+++ front-web/src/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import { ReactComponent as Logo } from './logo.svg';
+
+import './styles.css';
+
+function Navbar() {
+    return (
+        <nav className="main-navbar">
+            <Logo />
+            <a className="logo-text" href="home">DS Delivery</a>
+        </nav>
+    );
+}
+
+export default Navbar;
